3b18ba5bf4ec85d56da0893faa9fa76604bc732f
Abden Antony Fernandez(UST <291491@ust.com>
2025-04-22 16:53:34 +0530

    Promises done using modules

--- TS/Day 5/2_import.js
+++ TS/Day 5/2_import.js
@@ -2,3 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var _1_export_1 = require("./1_export");
 console.log((0, _1_export_1.multiply)(4, 5));
+var _1_export_2 = require("./1_export");
+console.log((0, _1_export_2.getHomeDetails)("Abden", 25));
+var _1_export_3 = require("./1_export");
+console.log((0, _1_export_3.getCarDetails)("BMW", 50000));
+// check a number is prime or not
+var _1_export_4 = require("./1_export");
+console.log((0, _1_export_4.isPrime)(7));
+console.log((0, _1_export_4.isPrime)(10));
+//Promise to fetch data from an API
+var _1_export_5 = require("./1_export");
+(0, _1_export_5.getFacts)("https://cataas.com/cat?width=200;height=200;json=true").then(function (data) {
+    console.log(data);
+}).catch(function (error) {
+    console.error("Error fetching facts:", error);
+});
