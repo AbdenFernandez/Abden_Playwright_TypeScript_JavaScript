6f4de61696d2791c7e3a9d8c431d185ae5ab8257
Abden Antony Fernandez(UST <291491@ust.com>
2025-04-22 16:25:06 +0530

    import/export

---
+++ TS/Day 5/2_import.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _1_export_1 = require("./1_export");
+console.log((0, _1_export_1.multiply)(4, 5));
---
+++ TS/Day 5/2_import.ts
@@ -0,0 +1,2 @@
+import { multiply as mul } from "./1_export";
+console.log(mul(4, 5));
