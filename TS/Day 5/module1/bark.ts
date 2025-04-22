7d1e5bde9ace69d828773f7a79a431dbb4b18683
Abden Antony Fernandez(UST <291491@ust.com>
2025-04-22 16:07:46 +0530

    synopsis

--- TS/Day 5/module1/bark.ts
+++ TS/Day 5/module1/bark.ts
@@ -1,3 +1,16 @@
 export default function speak() {
     return "bark";
 }
+
+//mathUtils.ts(a separate file)
+export function add(a: number, b: number): number {
+    return a + b;
+}
+export const PI = 3.14;
+
+
+//app.ts
+import { add, PI } from './mathUtils';
+let result = add(10, 5);
+console.log(`Result: ${result}`);
+console.log(`Value of PI: ${PI}`);
