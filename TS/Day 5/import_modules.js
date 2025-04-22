60e7bd8b3887cde9e07818b7461fae2accdeb726
Abden Antony Fernandez(UST <291491@ust.com>
2025-04-22 15:56:24 +0530

    import and export done

---
+++ TS/Day 5/import_modules.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var modules_ex_1 = require("./modules_ex");
+var bark_1 = require("./module1/bark");
+console.log((0, modules_ex_1.name1)("Abden"));
+console.log((0, bark_1.default)());
---
+++ TS/Day 5/import_modules.ts
@@ -0,0 +1,5 @@
+import {name1, A} from "./modules_ex";
+import speak from "./module1/bark"
+
+console.log(name1("Abden"));
+console.log(speak());
