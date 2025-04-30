41051dd3d3e7e55316501483e692a44127bef62d
Abden Antony Fernandez(UST <291491@ust.com>
2025-04-30 13:51:56 +0530

    Playwright TypeScript JavaScript

--- Playwright_UST/Day_2_Playwright/tests/allimportanthingd.spec.ts
+++ Playwright_UST/Day_2_Playwright/tests/allimportanthingd.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
- 
- 
- 
- 
+
+
+
+
 test('@Web Client App login', async ({ page }) => {
    //js file- Login js, DashboardPage
    const email = "anshika@gmail.com";
@@ -15,33 +15,36 @@ test('@Web Client App login', async ({ page }) => {
    await page.waitForLoadState('networkidle');
    await page.locator(".card-body b").first().waitFor();
    const titles = await page.locator(".card-body b").allTextContents();
-   console.log(titles); 
- 
+   console.log(titles);
+
 })
 
 
-test('@Child windows hadl', async ({browser})=>
-    {
-       const context = await browser.newContext();
-       const page =  await context.newPage();
-       const userName = page.locator('#username');
-       await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-       const documentLink = page.locator("[href*='documents-request']");
-    
-       const [newPage]=await Promise.all(
+test('@Child windows hadl', async ({ browser }) => {
+   const context = await browser.newContext();
+   const page = await context.newPage();
+   const userName = page.locator('#username');
+   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+   const documentLink = page.locator("[href*='documents-request']");
+
+   const [newPage] = await Promise.all(
       [
          context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
          documentLink.click(),
-      
+
       ])//new page is opened
-      
-    
-      const  text = await newPage.locator(".red").textContent();
-       const arrayText = text.split("@")
-       const domain =  arrayText[1].split(" ")[0]
-       console.log(domain);
-       await page.locator("#username").fill(domain);
-       console.log(await page.locator("#username").textContent());
-    
-    })
-    
+
+
+   const text = await newPage.locator(".red").textContent();
+   if (text) {
+      const arrayText = text.split("@");
+      const domain = arrayText[1].split(" ")[0];
+      console.log(domain);
+      await page.locator("#username").fill(domain);
+      console.log(await page.locator("#username").textContent());
+   } else {
+      console.error("The element with selector '.red' has no text content.");
+   }
+
+
+})
