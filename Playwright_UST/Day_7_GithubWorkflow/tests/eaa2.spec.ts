c0b34b3f0f017b9b112478db088428ec86e52748
Abden Antony Fernandez(UST <291491@ust.com>
2025-05-02 17:20:13 +0530

    Github workflows and authentication with playwright

--- Playwright_UST/Day_7_GithubWorkflow/tests/eaa2.spec.ts
+++ Playwright_UST/Day_7_GithubWorkflow/tests/eaa2.spec.ts
@@ -1,8 +1,5 @@
 import { test, expect } from '@playwright/test';
-
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.goto('https://github.com/');
+  await expect(await page.locator('#dashboard')).toBeVisible();
 });
